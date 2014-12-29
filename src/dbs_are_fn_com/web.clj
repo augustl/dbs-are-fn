b06f803856c92a50c939c155ebd5f430d49c0bb5
August Lilleaas <august@augustl.com>
2014-12-29 21:55:07 +0100

    More styling tweaks.

--- src/dbs_are_fn_com/web.clj
+++ src/dbs_are_fn_com/web.clj
@@ -12,6 +12,7 @@
       [:head
        [:meta {:charset "utf-8"}]
        [:title (if (nil? title) base-title (str title " (" base-title ")"))]
+       [:link {:href "/stylesheets/reset.css" :media "screen" :rel "stylesheet" :type "text/css"}]
        [:link {:href "/stylesheets/screen.css" :media "screen" :rel "stylesheet" :type "text/css"}]
        [:link {:href "/atom.xml" :rel "alternate" :title base-title :type "application/atom+xml"}]]
       [:body
@@ -19,27 +20,31 @@
         [:div {:class "site-content"}
          [:a.site-title {:href "/"} base-title]
          [:a.site-subtitle {:href "/"} "A blog about functional databases"]]]
-       [:div {:class "site-content site-content-main"} page]])))
+       [:div {} page]])))
 
 (defn get-home-page [posts req]
   (layout-page
-   (list
-    (map
-     (fn [{:keys [url headers pretty-date]}]
-       [:div.listed-post
-        [:a {:href url} (:title headers)]
-        " "
-        [:span.listing-date pretty-date]])
-     posts)
-    [:p "This blog is served as static HTML, generated using " [:a {:href "https://github.com/magnars/stasis"} "Stasis"] ", a Clojure library. The site is " [:a {:href "https://github.com/augustl/dbs-are-fn.com"} "open source"] "."])))
+   [:div {:class "site-content"}
+    (list
+     (map
+      (fn [{:keys [url headers pretty-date]}]
+        [:div.listed-post
+         [:a {:href url} (:title headers)]
+         " "
+         [:span.listing-date pretty-date]])
+      posts)
+     [:p "This blog is served as static HTML, generated using " [:a {:href "https://github.com/magnars/stasis"} "Stasis"] ", a Clojure library. The site is " [:a {:href "https://github.com/augustl/dbs-are-fn.com"} "open source"] "."])]))
 
 (defn layout-post
   [{:keys [get-body headers pretty-date]}]
   (layout-page
    (list
-    [:h1 (:title headers)]
-    [:p.post-timestamp pretty-date]
-    (get-body))
+    [:div {:class "post-title"}
+     [:div {:class "site-content"}
+      [:h1 (:title headers)]]]
+    [:div {:class "site-content"}
+     [:p.post-timestamp pretty-date]
+     [:div.post-body (get-body)]])
    (:title headers)))
 
 (defn get-assets []
